0b55c90dda94eb0da0ca878567a1fda7840e9f9f
dizere517 <140904150+dizere517@users.noreply.github.com>
2025-12-24 03:56:40 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,9 @@
 <?php
 $host = getenv("DB_HOST");
-$port = intval(getenv("DB_PORT") ?: 3306);
+$port = intval(getenv("DB_PORT"));
 $db   = getenv("DB_NAME");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
-
-if (!$host || !$db || !$user) {
-  die("Config DB manquante: vérifie les variables d'environnement DB_HOST/DB_NAME/DB_USER/DB_PASS.");
-}
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli($host, $user, $pass, $db, $port);
