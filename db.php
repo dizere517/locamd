a1ec03ce90e8b7ee91ae65020a08ded686a363a2
dizere517 <140904150+dizere517@users.noreply.github.com>
2025-12-24 04:15:34 +0100

    Fix database port retrieval in db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 $host = getenv("DB_HOST");
-$port = intval(getenv("DB_PORT"));
+$port = intval(getenv("3306"));
 $db   = getenv("DB_NAME");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
@@ -9,3 +9,4 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli($host, $user, $pass, $db, $port);
 $conn->set_charset("utf8mb4");
 ?>
+
