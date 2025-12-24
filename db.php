cffb3afa8a7099cc687bf299f36f6dcbead67262
dizere517 <140904150+dizere517@users.noreply.github.com>
2025-12-24 02:29:12 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 $servername = "localhost";
-$port       = 3307;
 $username   = "root";        // change si besoin
 $password   = "";            // change si besoin
 $dbname     = "depannage_cdl";
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connexion échouée: " . $conn->connect_error);
 }
+
 ?>
