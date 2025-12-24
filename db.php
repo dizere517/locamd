11d14e4c4e1b2e203ec0249e4b4fbe9f7fa447f5
dizere517 <140904150+dizere517@users.noreply.github.com>
2025-12-24 03:08:38 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,11 @@
 <?php
-$servername = getenv("DB_HOST") ?: "localhost";
-$username   = getenv("DB_USER") ?: "root";
-$password   = getenv("DB_PASS") ?: "";
-$dbname     = getenv("DB_NAME") ?: "depannage_cdl";
-$port       = intval(getenv("DB_PORT") ?: 3306);
+$host = getenv("DB_HOST") ?: "localhost";
+$port = intval(getenv("DB_PORT") ?: 3306);
+$db   = getenv("DB_NAME") ?: "depannage_cdl";
+$user = getenv("DB_USER") ?: "root";
+$pass = getenv("DB_PASS") ?: "";
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 $conn->set_charset("utf8mb4");
-?>
